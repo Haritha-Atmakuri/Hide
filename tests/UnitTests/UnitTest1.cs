a239584b1193e7e46f085badf464f31bc504336e
Haritha-Atmakuri <harithaatmakuri1105@gmail.com>
2019-02-08 00:07:30 -0600

    added some test function

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -8,7 +8,24 @@ namespace UnitTests
         [Fact]
         public void Test1()
         {
-
+            bool expected = false
+            var age = 14
+         bool actual =  checkAge(age)
+         Assert.Equal(expected,actual);
         }
+    
+
+    bool checkAge(int age)
+    {
+      if(age > 18)
+      {
+          return true
+      }
+      else
+      {
+          return false
+      }
+    }
+
     }
 }
